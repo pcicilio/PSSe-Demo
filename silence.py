4186c5952fb98e3ff0ab506eb022c10499b45728
pcicilio <phylicia.r.cicilio@gmail.com>
2021-12-06 18:48:57 -0900

    Added code

---
+++ silence.py
@@ -0,0 +1,22 @@
+#Used to silence some of PSSE output without automatically silencing all
+
+from __future__ import with_statement
+from contextlib import contextmanager
+import sys
+import os
+
+@contextmanager
+def silence(file_object=None):
+    """
+    Discard stdout (i.e. write to null device) or
+    optionally write to given file-like object.
+    """
+    if file_object is None:
+        file_object = open(os.devnull, 'w')
+
+    old_stdout = sys.stdout
+    try:
+        sys.stdout = file_object
+        yield
+    finally:
+        sys.stdout = old_stdout
